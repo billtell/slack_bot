68b14a58d5669c37b06a5265df4ccb187d33f587
Bill Teller <gteller@wustl.edu>
2023-05-11 16:40:44 -0500

    add jsonify

--- langchain-experiments-main/slack/app.py
+++ langchain-experiments-main/slack/app.py
@@ -4,7 +4,7 @@ from slack_sdk.errors import SlackApiError
 from slack_bolt.adapter.flask import SlackRequestHandler
 from slack_bolt import App
 from dotenv import find_dotenv, load_dotenv
-from flask import Flask, request
+from flask import Flask, request, jsonify
 from functions import draft_email
 
 # Load environment variables from .env file
